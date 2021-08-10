a635a94a2b325e20428d4198a43dc6984581badc
t572wai <53143353+t572wai@users.noreply.github.com>
2021-08-11 02:25:11 +0900

    ref(tile->mino)

--- display.js
+++ display.js
@@ -83,7 +83,7 @@ function removeGhostTiles() {
 }
 
 function displayTile(tile) {
-	$('.ghostMinos[data-x="'+tile[0]+'"][data-y="'+tile[1]+'"]').attr('class','minos '+tile[2]["string"]+"GhostMinos");
+	$('.minos[data-x="'+tile[0]+'"][data-y="'+tile[1]+'"]').attr('class','minos '+tile[2]["string"]+"Minos");
 }
 function displayGhostTile(tile) {
 	if (tile[1]<2) {
@@ -93,7 +93,7 @@ function displayGhostTile(tile) {
 	$('.minos[data-x="'+tile[0]+'"][data-y="'+tile[1]+'"]').html(ghostText);
 }
 function removeGhostTile(tile) {
-	$('.minos[data-x="'+tile[0]+'"][data-y="'+tile[1]+'"]').html("");
+	$('.ghostMinos[data-x="'+tile[0]+'"][data-y="'+tile[1]+'"]').html("");
 }
 
 function displayDifferWithDelay(differs,callback) {
