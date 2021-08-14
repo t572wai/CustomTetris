75a633980b32b9a9663e038fefd5041532cf1d7b
t572wai <53143353+t572wai@users.noreply.github.com>
2021-08-14 22:37:28 +0900

    import順変更

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ import "./master.scss";
 import "./media.scss";
 import "./tetriminos.scss";
 
-import "./const.ts";
-import "./dialogs.ts";
-import "./display.ts";
 import "./enum.ts";
+
+import "./const.ts";
 import "./general.ts";
-import "./global.ts";
-import "./init.ts";
 import "./keyinput.ts";
-import "./sounds.ts";
 import "./SwiperClass.ts";
-import "./systems.ts";
-import "./tetriminos.ts";
-import "./tetrisGameType.ts";
+
 import "./tetrisKeyinput.ts";
+import "./tetrisGameType.ts";
+import "./sounds.ts";
+import "./dialogs.ts";
+import "./global.ts";
+import "./display.ts";
+import "./tetriminos.ts";
+import "./systems.ts";
+
+import "./init.ts";
