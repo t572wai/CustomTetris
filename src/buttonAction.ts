d6d47e559f7bb6e107306291c5c2c5a7b210cb40
t572wai <53143353+t572wai@users.noreply.github.com>
2021-08-17 23:37:54 +0900

    obj.onからdocument.on(objに

--- src/buttonAction.ts
+++ src/buttonAction.ts
@@ -3,26 +3,31 @@ let pressInterval = new Map<string, NodeJS.Timer>();
 let pressDidLongpressed = new Map<string, boolean>();
 
 function setButtonActions(obj: string, waitSec: number, intervalSec: number) {
-	let target = $(obj);
+	//let target = $(obj);
 	//console.log(obj,target);
 	$(document).on('mousedown touchstart', obj, function (e) {
-		target.trigger('pressstart');
+		//target.trigger('pressstart');
+		$(document).trigger('pressstart', obj);
 		pressDidLongpressed.set(obj, false);
 		console.log('pressstart');
 		pressTimer.set(obj, setTimeout(function () {
-			target.trigger('longpress');
+			//target.trigger('longpress');
+			$(document).trigger('longpress', obj);
 			pressDidLongpressed.set(obj, true);
 			pressInterval.set(obj, setInterval(function () {
 				console.log('longpress');
-				target.trigger('longpress')
+				$(document).trigger('longpress', obj);
+				//target.trigger('longpress')
 			}, intervalSec))
 		}, waitSec))
 	})
 
 	$(document).on('mouseup touchend', obj, function (e) {
-		target.trigger('pressend');
+		//target.trigger('pressend');
+		$(document).trigger('pressend', obj);
 		if (pressDidLongpressed.get(obj)) {
-			target.trigger('shortpress');
+			//target.trigger('shortpress');
+			$(document).trigger('shortpress', obj);
 		}
 		clearTimeout(pressTimer.get(obj)!);
 		clearInterval(pressInterval.get(obj)!);
