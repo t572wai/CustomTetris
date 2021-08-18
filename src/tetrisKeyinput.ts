bb4e88be1335c5a0003032e2c4e62f200b6d7ad9
t572wai <53143353+t572wai@users.noreply.github.com>
2021-08-18 13:15:27 +0900

    スクロールと右クリック禁止

--- src/tetrisKeyinput.ts
+++ src/tetrisKeyinput.ts
@@ -6,9 +6,12 @@ type Operate = typeof Operations[number];
 let keyBinding = new Map<Operate, string>();
 
 document.oncontextmenu = function () {return false;}
-document.body.addEventListener('touchmove', function (e) {
+document.body.oncontextmenu = () => {
+	return false;
+}
+document.addEventListener('touchmove', function (e) {
 	e.preventDefault();
-}, false)
+}, {passive: false})
 
 function addRightKeyActions(key: string): void {
 	addKeyActions(key, onRight, () => {}, onRight, () => {}, 300, 50);
