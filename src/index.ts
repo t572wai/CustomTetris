5e5d3cc7c32c064a35ffa23d336fe8fb1199f205
t572wai <53143353+t572wai@users.noreply.github.com>
2021-08-26 22:27:31 +0900

    gameOptionsのmodule化, GameRuleOptionのtiltleをtitleに

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@ import { startSound, lockDownSound } from './sounds';
 import { Tetrimino, Pos, Mino, normalBufferHeight, normalFieldHeight, normalFieldWidth, TetriminoEnum } from "./global";
 import { ChangeSizeOfMatrix, ChangeStyle, GameRule } from './gameRule';
 import { TimerOfAbilityToEsc } from "./timerOfAbilityToEsc";
+import { GameOption } from "./gameOptions";
 
 //
 //
@@ -575,48 +576,6 @@ $(function () {
 
 //const options = ['GameRule']
 
-class GameOption<T> {
-	private _optionName: string;
-	private _currentOption: T;
-	private _enumOfT: Enum<T>;
-
-	constructor(name:string, indOfDefault: number=0, enumOfT: Enum<T>) {
-		this._optionName = name;
-		this._enumOfT = enumOfT;
-		this._currentOption = this._enumOfT.defArray[indOfDefault];
-
-		$(document).on('change', 'input[name="'+this._optionName+'"]', (e) => {
-			const value = $('input[name="'+this._optionName+'"]:checked').val() as number;
-			const value_T = this._enumOfT.defArray[value];
-			//console.log(value);
-			if (typeof value_T !== 'undefined') {
-				this._currentOption = value_T;
-			}
-		})
-	}
-
-	get currentOption(): T {
-		return this._currentOption;
-
-}
-	displayRadioOption(obj: string): void {
-		let htmlText = "<div id='"+this._optionName+"RadioContainer'>";
-		for (let i = 0; i < this._enumOfT.defArray.length; i++) {
-			const option = this._enumOfT.defArray[i];
-			//console.log(this._enumOfT.toString(option));
-			htmlText += `
-				<div class='radio'>
-					<input type='radio' name='${this._optionName}' value='${i}' id='${this._optionName}-${this._enumOfT.toString(option)}'>
-					<label class='radio-label' for='${this._optionName}-${this._enumOfT.toString(option)}'>${this._enumOfT.getTitle(option)}</label>
-				</div>
-			`
-		}
-		$(obj).append(htmlText);
-		const currentIndex =this._enumOfT.defArray.lastIndexOf(this._currentOption).toString();
-		//console.log(currentIndex);
-		$(obj+' input[name="'+this._optionName+'"]').val([currentIndex]);
-	}
-}
 
 //
 //
@@ -676,7 +635,7 @@ const EnumOfGameRule:Enum<GameRule> = {
 	defArray: GameRules,
 	toEnum: toGameRule,
 	toString: GameRule.toString,
-	getTitle: GameRule.toString,
+	getTitle: GameRule.getTitle,
 }
 
 function toString(arg: string): string {
