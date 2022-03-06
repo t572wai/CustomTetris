52c88fd8afc69917ab788f5aab17c5b2d7c3c60a
waiyangar572 <93606715+waiyangar572@users.noreply.github.com>
2022-03-06 16:37:49 +0900

    shaft-dif=pos

--- src/tetris.ts
+++ src/tetris.ts
@@ -638,7 +638,7 @@ export class Tetris {
 	}
 	rotate(direction: 1|3): boolean {
 		const dif = this.getDifOfShaft((this._currentFacing+direction)%4 as 0|1|2|3);
-		const following = getMovedMinos(getRotatedMinos(this.currentMinos(), this.getShaft(), direction), dif.x, dif.y);
+		const following = getMovedMinos(getRotatedMinos(this.currentMinos(), this.getShaft(), direction), -dif.x, -dif.y);
 		console.log("shaft",this.getShaft(),"current",this.currentMinos(),"following",following);
 		if (this.canMove(following)) {
 			this.relocate(following);
