0a5db35901ae2a5d117ffe06bc5e876bb86be85a
waiyangar572 <93606715+waiyangar572@users.noreply.github.com>
2022-03-06 18:44:26 +0900

    currentMinosをcurrentPosで回転

--- src/tetris.ts
+++ src/tetris.ts
@@ -408,7 +408,7 @@ export class Tetris {
 	currentMinos(): Mino[] {
 		const minoBase = Tetris.replaceMinoType(this._gameRule.tetriminoClass.getTetriminoShape(this._currentMinoShape)!,this._currentMinoType)
 		const dif = this.getDifOfShaft(this._currentFacing);
-		console.log(getMovedMinos(minoBase, this._currentPos.x, this._currentPos.y),getRotatedMinos(getMovedMinos(minoBase, this._currentPos.x, this._currentPos.y), this.getShaft(), this._currentFacing),getMovedMinos(getRotatedMinos(getMovedMinos(minoBase, this._currentPos.x, this._currentPos.y), this.getShaft(), this._currentFacing), dif.x, dif.y));
+		console.log(getMovedMinos(minoBase, this._currentPos.x, this._currentPos.y),getRotatedMinos(getMovedMinos(minoBase, this._currentPos.x, this._currentPos.y), this._currentPos, this._currentFacing),getMovedMinos(getRotatedMinos(getMovedMinos(minoBase, this._currentPos.x, this._currentPos.y), this.getShaft(), this._currentFacing), dif.x, dif.y));
 		
 		return getMovedMinos(getRotatedMinos(getMovedMinos(minoBase, this._currentPos.x, this._currentPos.y), this.getShaft(), this._currentFacing), dif.x, dif.y);
 	}
