77c9daa65eb464e6e1accde37bea6be691207700
waiyangar572 <93606715+waiyangar572@users.noreply.github.com>
2022-04-01 23:05:41 +0900

    log

--- src/tetris.ts
+++ src/tetris.ts
@@ -476,6 +476,8 @@ export class Tetris {
 	
 	
 	isOtherTiles(tile: Mino | Pos): boolean {
+		console.log(tile, this._fieldArray);
+		
 		if (this._gameRule.tetriminoClass.attrMap.get(this._fieldArray[tile.y][tile.x]) != 'empty') {
 			console.log(this.isTetriminoVisible(),this.currentMinos().some((element) => {return element.x==tile.x && element.y==tile.y }));
 			
